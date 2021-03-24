ec1a193fc7bf7f1890b49deadab231106002a708
Yeong Do <parand0320@beyless.com>
2021-03-24 11:55:08 +0900

    [FEAT] test code for reading sn

--- main.cpp
+++ main.cpp
@@ -3,11 +3,18 @@
 #include "debug.h"
 #include <stdlib.h>
 #include "sound_manager.h"
+#include "vendor_storage.h"
+
 std::string get_target_board_name(){
     //TODO: 하드코딩된 부분을 디바이스 정보를 읽어서 구현하는 방식으로 해야 함
     return "DEEPTHINK";
 }
 
+std::string read_sn(){
+    char data_0[128] = {0};
+    rkvendor_read(VENDOR_SN_ID, data_0, sizeof(data_0)/sizeof(data_0[0]));
+	fprintf(stderr, "DEMO-%d: read vendor sn: %s\n", __LINE__, data_0);
+}
 // Door lock controll pin numbers.. (up to device setting)
 
 /*
@@ -29,7 +36,7 @@ int main(int argc, char** argv) {
 #endif
     //get board info
     std::string target_board= get_target_board_name();
-
+    read_sn();
     //create terminal object and initialize mqtt, MySQl
     terminal T(broker_ip, 0, device_id, device_id, target_board);
     T.initialize_mqtt_client();
