dce274856cc82ef4697ddfc7af354dde504cfb37
Yeong Do <parand0320@beyless.com>
2021-04-28 16:17:17 +0900

    [FEAT] added save log as file feature

--- logger.cpp
+++ logger.cpp
@@ -3,10 +3,15 @@
 //
 
 #include "logger.h"
+#include <iostream>
+#include <fstream>
 
 // default initializer with log host
 logger::logger(std::string _log_host) {
     this->log_host = _log_host;
+    this->log_file_name_ = LOGFILE_ROOT_PATH + "log/" + currentDateTime() + "_log.txt";
+    this->cfg_file_name_ = LOGFILE_ROOT_PATH + "log/log_enable.cfg";
+    this->save_log = get_savelog_config();
 }
 
 
@@ -27,5 +32,19 @@ void logger::print_log(std::string log_body) {
     char out_log[1000];
     sprintf(out_log, "[%s][%s] %s", currentDateTime().c_str(), this->log_host.c_str(), log_body.c_str());
     std::cout << out_log << std::endl;
+    if(this->save_log){
+        save_log_to_file(out_log);
+    } 
 }
 
+void logger::save_log_to_file(const char *log){
+    std::ofstream log_stream;
+    log_stream.open(log_file_name_,std::ios_base::out | std::ios_base::app);
+    log_stream << log << std::endl;
+    log_stream.close();
+}
+
+bool logger::get_savelog_config(){
+    std::ifstream file_stream(this->cfg_file_name_);
+    return file_stream.is_open();
+}
--- logger.h
+++ logger.h
@@ -11,6 +11,14 @@
 class logger {
 private:
     std::string log_host; // log host
+
+    const std::string LOGFILE_ROOT_PATH = "/mnt/d/Beyless/0.project/2.firefly_rk3399/src/beyless_vending_terminal/";
+    //const std::string LOGFILE_ROOT_PATH = "/home/firefly/beyless_vending_terminal/";
+    std::string log_file_name_;
+    std::string cfg_file_name_;
+    void save_log_to_file(const char *log);
+    bool get_savelog_config();
+    bool save_log;
 public:
     logger(std::string _log_host); // default initializer with log host
 
