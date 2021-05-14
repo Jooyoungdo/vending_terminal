eea2052e565d05c16c580508afe0141f0aedf365
Yeong Do <parand0320@beyless.com>
2021-05-14 16:41:21 +0900

    [FIX] fixed home variable

--- start_baive.sh
+++ start_baive.sh
@@ -1,5 +1,3 @@
 sleep 10
-## 현재 daemon_process를 crontab으로 실행 시 권한이 없어서 image write를 못하는 이슈가 있음
-## 이 부분 수정해야 함 
-sudo $HOME/beyless_vending_terminal/bin/daemon_process 20002 ai0.beyless.com:1883 &
-$HOME/beyless_vending_terminal/baive_ad/baive-ad.AppImage --id=20002 --url=https://ai0.beyless.com/baive &
+sudo /home/firefly/beyless_vending_terminal/bin/daemon_process 20002 ai0.beyless.com:1883 &
+/home/firefly/beyless_vending_terminal/baive_ad/baive-ad.AppImage --id=20002 --url=https://ai0.beyless.com/baive &
